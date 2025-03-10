7db87b66aad5f39362505f2628c7000fe2ad6b1d
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-10 09:39:51 -0700

    Javascript

---
+++ firstproject.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("Hello World");
+const num = 42;
+console.log(`The answer is ${num}`);
+
+<script>
+    document.write("Getting started with JavaScript tutorial");
+</script>
