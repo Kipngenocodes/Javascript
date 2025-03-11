a0da59e5979e782abc1feb265835cf450ce414d0
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-11 02:36:01 -0700

    working on serverside messages

--- firstproject.js
+++ firstproject.js
@@ -2,3 +2,8 @@ console.log("Hello World");
 console.log("Hello World");
 const num = 42;
 console.log(`The answer is ${num}`);
+
+// writing Hello World to the console
+console.log("Hello World");
+;
+
