9bf06ea3f4dc66a1d9f8bcf1c831c7078330ecc2
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-13 06:21:09 -0700

    working on variable hoisting

---
+++ vrhoisting.js
@@ -0,0 +1,10 @@
+function test() {
+    // Hoisiting is not supported by let keyword
+    // m = 100;
+    // document.write("m = " + m + "<br/>");
+    // let m;
+    n = 50;
+    console.log("n = " + n );
+    var n;
+ }
+ test();
