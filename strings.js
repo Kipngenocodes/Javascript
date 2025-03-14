ccf26af74b25f316273ce01c0c7b9618733f58ff
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-13 20:05:01 -0700

    working with strings

---
+++ strings.js
@@ -0,0 +1,6 @@
+let str1 = "Hello World!"; // Using double quotes
+let str2 = 'Hello World!'; // Using single quotes
+let str3 = `Hello World!`; // Using backticks
+console.log(str1 + "<br>");
+console.log(str2 + "<br>");
+console.log(str3 + "<br>");
