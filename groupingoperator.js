6a4814ebfaf37f8c86e81989ddb3b5c954935dbb
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-14 23:14:01 -0700

    yeild operator

---
+++ groupingoperator.js
@@ -0,0 +1,10 @@
+/*  The grouping operator in JavaScript controls
+ the precedence of the evaluation in expressions. */
+
+// Example 1: Using grouping operator to control precedence
+let result1 = (2 + 3) * 4; // (2 + 3) is evaluated first, then the result is multiplied by 4
+console.log(result1); // Output: 20
+
+// Example 2: Using grouping operator to control precedence
+let result2 = 2 + 3 * 4; // 3 * 4 is evaluated first, then the result is added to 2
+console.log(result2); // Output: 14                                                         
