1520305108814708d3a0c9b91c3d27203bcca959
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-14 23:59:52 -0700

    working  on yeild operator

--- yeildoperator.js
+++ yeildoperator.js
@@ -17,3 +17,22 @@ const generator = generateSequence();
 console.log(generator.next().value); // Output: 1
 console.log(generator.next().value); // Output: 2
 console.log(generator.next().value); // Output: 3
+
+function* test() {
+    console.log("I'm before yield expression");
+    yield 20;
+}
+const genObj = test();
+console.log(genObj.next());
+
+
+function* test() {
+    yield 20;
+    yield [1,2,3];
+    yield "Hello World";
+}
+let res = test();
+console.log(res.next());
+console.log(res.next());
+console.log(res.next());
+console.log(res.next());
