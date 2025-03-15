e654cab49077543f01aba5dda9414f941869b2ab
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-15 00:12:47 -0700

    working on spread operator

---
+++ spreadoperator.js
@@ -0,0 +1,6 @@
+/* use cases of the spread operator are to copy array elements,
+ concatenate arrays or objects with rest parameters*/
+
+ let x = ["Tutorials", "Point"];
+console.log(x); // [ 'Tutorials', 'Point' ]
+console.log(...x); // Tutorials Point
