d5541764ebbf5757d151dcd66200f46f1adbf592
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-16 02:22:45 -0700

    updates

---
+++ Javascript_Func/func.js
@@ -0,0 +1,14 @@
+/* syntax for functions
+function functionName(parameter1, parameter2) {
+
+    return parameter1 + parameter2; // Example: return the sum of two parameters
+}
+*/
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+let result = addNumbers(13, 50);
+console.log(result); // Output: 8
+ 
