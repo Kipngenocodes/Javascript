342096d91f9d9d7dd455fbaec44b6e6fc9818429
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-20 02:58:01 -0700

    working with arrow functions

---
+++ Javascript_Func/Arrow_Functions.js
@@ -0,0 +1,11 @@
+// The arrow functions in JavaScript allow us to create a shorter and anonymous function.
+// Arrow functions are often used in combination with higher-order functions, which are functions that take other functions as arguments or return functions as their result.   
+// Arrow functions are not hoisted, so they must be defined before they are called.
+
+/* Syntax :
+const varName = (parameters) => {
+     function body
+};
+*/
+
+
