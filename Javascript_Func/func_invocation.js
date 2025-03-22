f88d3204a653242ca50795d668365a7fc172e824
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-22 00:04:30 -0700

    function invocation in Javascript

---
+++ Javascript_Func/func_invocation.js
@@ -0,0 +1,8 @@
+// function invocation in javascript 
+
+    // Function declaration
+    function greet() {
+        console.log('Hello, World!');
+    }   
+    greet(); // Output: Hello, World!
+
