44be55cfab5e724619423c0c342b9d27627a5652
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-21 23:12:09 -0700

    working  with arrow functions inJavascript

--- Javascript_Func/Arrow_Functions.js
+++ Javascript_Func/Arrow_Functions.js
@@ -6,6 +6,33 @@
 const varName = (parameters) => {
      function body
 };
+
+or 
+const Varname = (parameters) => function body;
 */
 
 
+// An Example of arrow functions that take other functions as arguments or return functions as their result
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const calculate = (operation, a, b) => operation(a, b);
+
+console.log(calculate(add, 2, 3)); // Output: 5
+console.log(calculate(multiply, 2, 3)); // Output: 6   
+
+// Arrow Function with Multiple Statements
+const greet = (name) => {
+    const greeting = `Hello, ${name}!`;
+    console.log(greeting);
+    return greeting;
+};
+
+greet('John'); // Output: Hello, John!
+
+// Arrow Function with Multiple Statements with integer return type
+const addition = (a, b) => {
+    const sum = a + b;
+    return sum;
+};
+
+console.log(addition(5, 3)); // Output: 8
