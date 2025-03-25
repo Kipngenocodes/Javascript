f3a608c6529a4503fc45310dbc404f2b176d03c8
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-24 21:18:00 -0700

    working with strings objects

---
+++ objects/strings_objects.js
@@ -0,0 +1,16 @@
+// The String object in JavaScript lets you work with a series of characters; 
+// it wraps JavaScript's string primitive data type with a number of helper methods.
+
+// syntax for creating a string object:
+// const val = new String('string');
+
+//Example
+str1 = 'Hello World!'; // using single quote
+str2 = "Hello World!"; // using double quote
+str3 = 'Hello World'; // using back ticks
+str4 = String('Hello World!'); // using String() function
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
