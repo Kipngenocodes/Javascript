dd51606a435c2b3171c8c2851c75d8020762eb51
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-24 21:08:46 -0700

    working with boolean objects

---
+++ objects/boolean_objects.js
@@ -0,0 +1,14 @@
+// JavaScript Boolean object represents two values, either "true" or "false".
+// If value parameter is omitted or is 0, -0, null, false, NaN, undefined,
+// or the empty string (""), the object has an initial value of false. In programming,
+// the if-else statement evaluates either the code of the 'if' block or the 'else' block based
+// on the boolean value of the conditional expression
+
+/* Syntax for creating a boolean object: const val = new Boolean(value);*/
+// Creating a boolean object
+const val = new Boolean(true);
+console.log(val); // Output: true
+
+// Creating a boolean object with false value
+const val1 = new Boolean(false);
+console.log(val1); // Output: false
