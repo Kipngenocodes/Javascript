d20b871691b29faa183f29875cf43c97240c9742
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-25 11:56:35 -0700

    working on Dataview

---
+++ objects/dataview.js
@@ -0,0 +1,3 @@
+// TheDataView is an object in JavaScript that allows you to work with the binary data stored in theArrayBuffer. 
+// It provides a low-level interface for reading and writing number types in a binary ArrayBuffer.
+
