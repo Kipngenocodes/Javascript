3d70f7aaca9a9a988d8f64cbdf7339bababcf65e
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-26 17:36:43 -0700

    regular expressions

---
+++ Regular_expression/reg_expre.js
@@ -0,0 +1,19 @@
+// A regular expression (RegExp) in JavaScript is an object that describes a pattern of characters.
+/*It can contain the alphabetical, numeric, and special characters.
+ Also, the regular expression pattern can have single or multiple characters.
+
+The JavaScript RegExp class represents regular expressions, 
+and both String and RegExp define methods that use regular expressions
+to perform powerful pattern-matching and search-and-replace functions on text.*/
+
+
+// There are two ways to construct the regular expression in JavaScript.
+// 1.  Using the RegExp() constructor.
+// 2.  Using the regular expression literal.
+
+/* syntax for creating a regular expression using the RegExp() constructor:
+            var pattern = new RegExp(pattern, attributes);
+            or simply
+            var pattern = /pattern/attributes;
+*/
+
