ba7976d72ce62f01fe548b01b3d2993daccad755
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-26 17:08:30 -0700

    working with javascript math

---
+++ math/math.js
@@ -0,0 +1,27 @@
+// The JavaScript math object provides properties and methods for mathematical constants and functions.
+// All the properties and methods of Math are static and can be called by using Math as an object without creating it.
+
+// JavaScript Math Properties
+// Euler's constant and the base of natural logarithms, approximately 2.718.
+console.log("Euler's constant (e):", Math.E);
+
+// The natural logarithm of 10, approximately 2.302.
+console.log("Natural logarithm of 10:", Math.LN10);
+
+// The natural logarithm of 2, approximately 1.442.
+console.log("Natural logarithm of 2:", Math.LN2);
+
+// The base-10 logarithm of 1, approximately 0.434.
+console.log("Base-10 logarithm of 1:", Math.LOG10E);
+
+// The base-2 logarithm of 1, approximately 1.585.
+console.log("Base-2 logarithm of 1:", Math.LOG2E);
+
+// Pi, approximately 3.141.
+console.log("Pi:", Math.PI);    
+
+// Square root of 2, approximately 1.414.
+console.log("Square root of 2:", Math.SQRT2);
+
+// sqrt1_2 is the reciprocal of the square root of 2, approximately 0.707.
+console.log("Reciprocal of the square root of 2:", Math.SQRT1_2);
