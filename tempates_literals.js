cb872da313c90d08732ee1494686a8f6414f7e3f
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-30 23:23:10 -0700

    working with literal templates

---
+++ tempates_literals.js
@@ -0,0 +1,11 @@
+/*he template literals are introduced in ES6 to customize the string dynamically.
+ The template literals allow you to add variables or expressions into the string, 
+and the string changes according to the variables and expression value changes.
+*/
+// syntax for templates literals : let str = `Hi ${name}`;
+
+
+// Example 1: Using template literals to create a string with variables
+let name = "John";
+let greeting = `Hello, ${name}!`;
+console.log(greeting);  // Output: Hello, John!
