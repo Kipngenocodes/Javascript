e025b3ee34533b28bc3de15fb20e2255f145fb85
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-31 15:01:31 -0700

    working with object methods

--- classes/object_methods.js
+++ classes/object_methods.js
@@ -16,3 +16,31 @@ const person = {
 
 console.log(person.fullName());
 
+// Updating or Adding a Method to the Object
+person.fullName = function() {
+    return this.firstName + " " + this.lastName + " " + this.id + " updated";   
+
+}
+
+console.log(person.fullName());
+
+// Deleting a Method from the Object
+delete person.fullName;
+console.log(person.fullName); // Output: undefined
+console.log(person.firstName); // Output: John      
+
+// Example of a method that accepts parameters
+const person = {
+    name: "John",   
+    age: 30,
+    city: "New York",
+    country: "USA",
+    country_of_birth: "India",
+    greet: function (name) {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old. I live in ${this.city}, ${this.country}. I was born in ${this.country_of_birth}.`);
+    }   
+}
+
+person.greet("Alice"); // Output: Hello, my name is John and I am 30 years old. I live in New York, USA. I was born in India.
+
+// Example of a method that returns a new object
