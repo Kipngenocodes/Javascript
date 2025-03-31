f008d3badc29d01f083852a4c94113883747baf9
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-03-30 23:45:45 -0700

    working with tagged templates in js

---
+++ tagged_templates.js
@@ -0,0 +1,11 @@
+/*The tagged templates in JavaScript are an advanced version of the template literals. 
+You can define the function to format the string and 
+use it with a template literal to format the string according to the functionality of the function.
+*/
+/*  syntax for tagged templates : 
+function format(str, exp1, exp2, ...expN) {
+    Format the string
+}
+
+let res = format`${exp1} abcd ${exp2}`;
+*/
