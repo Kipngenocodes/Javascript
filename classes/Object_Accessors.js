8708a9512c2400b8ee350868ad5abd1a52493c37
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-03 01:34:41 -0700

    working on object accessors

---
+++ classes/Object_Accessors.js
@@ -0,0 +1,7 @@
+// The object accessor properties in JavaScript are methods that get or set the value of an object. 
+// They are defined using the get and set keywords. Accessor properties are a powerful way to control how your objects are accessed and modified.
+
+/*Data properties: const obj = {
+    key: "value", // Data property
+}
+*/
