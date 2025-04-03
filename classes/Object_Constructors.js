9334609058e7249ac7b2bebf9336206f92d715be
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-03 02:02:14 -0700

    working with JavaScript Object Prototype

--- classes/Object_Constructors.js
+++ classes/Object_Constructors.js
@@ -22,3 +22,22 @@ console.log(person1.name); // Output: John
 console.log(person1.age); // Output: 30
 console.log(person1); // Output: Person { name: 'John', age: 30 }
 console.log(typeof person1); // Output: object
+
+// JavaScript Object Prototypes
+// An object prototype is a blueprint for creating objects in JavaScript.
+// It allows you to define properties and methods that can be shared across multiple objects.
+// The prototype is defined using the prototype keyword.
+// The prototype is used to create objects and set values if there are any object properties present.
+// syntax for object prototype: obj.prototype.name = value;
+
+// Example of using object prototypes in JavaScript
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);  
+}
+// Creating an instance of the Person object using the new keyword
+const person2 = new Person("Alice", 25);
+person2.greet(); // Output: Hello, my name is Alice and I am 25 years old.      
