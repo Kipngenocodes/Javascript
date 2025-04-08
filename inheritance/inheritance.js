3883a223069b8818a8b4c0172633d85e642d4b6b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-08 03:28:52 -0700

    working with inheritance in js

---
+++ inheritance/inheritance.js
@@ -0,0 +1,16 @@
+// The concept of inheritance in JavaScript allows the child class to inherit the properties 
+// and methods of the parent class. 
+// Inheritance is also a fundamental concept of object-oriented programming like encapsulation and polymorphism. 
+// It allows for code reusability and a hierarchical class structure. 
+// In JavaScript, inheritance is primarily achieved through the prototype chain,
+//  where objects inherit properties and methods from their prototypes.
+// In ES6, the `class` syntax was introduced, which provides a clearer and more concise way to create classes and handle inheritance.
+//  The `extends` keyword is used to create a child class that inherits
+//  from a parent class, and the `super` keyword is used to call the constructor of the parent class.
+
+// syntax for inheritance in JavaScript fir a simgle class: 
+// class ChildClass extends ParentClass {
+// Child  class body.
+//     }
+
+// Example of inheritance in JavaScript using ES6 classes:
