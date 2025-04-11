20d298d9801e98bbf3c4f0dd9f566dded7450f87
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-10 23:48:22 -0700

    deconstructing object assignment

--- Destructing/desc_array.js
+++ Destructing/desc_array.js
@@ -10,3 +10,15 @@ console.log(a); // Output: 1
 console.log(b); // Output: 2
 console.log(c); // Output: 3
 
+// Nested array destructuring
+const nestedArr = [1, [2, 3]];
+const [x, [y, z]] = nestedArr;
+console.log(x); // Output: 1
+console.log(y); // Output: 2
+console.log(z); // Output: 3    
+
+// Object destructuring Assignment  
+const person = { name: 'John', age: 30 };
+const { name, age } = person;
+console.log(name); // Output: 'John'
+console.log(age); // Output: 30
