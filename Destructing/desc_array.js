a7b24f33c8dbff5494f788f6fe9c7b57ec3ed44d
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-10 23:44:19 -0700

    deconstructing array

---
+++ Destructing/desc_array.js
@@ -0,0 +1,12 @@
+// Deconstructing Assignment 
+// Destructuring assignment is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+// It is a JavaScript expression that allows unpacking values from arrays or properties from objects into distinct variables.
+// It is a convenient way to extract values from arrays or properties from objects and assign them to variables in a single statement.
+
+// array destructuring
+const arr = [1, 2, 3];
+const [a, b, c] = arr;
+console.log(a); // Output: 1
+console.log(b); // Output: 2
+console.log(c); // Output: 3
+
