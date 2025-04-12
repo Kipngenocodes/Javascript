bdb5e1ef4be7a6b9494422cff5a3524d1f87e619
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-11 21:29:39 -0700

    working  with array destructing

---
+++ Destructing/array_deconstruct.js
@@ -0,0 +1,10 @@
+/*JavaScript Array destructuring allows us to unpack the values from array
+and assign them to the variables. After that, you can use the variables 
+to access their value and use them in the code. We can perform the array 
+structuring using the destructuring assignment. 
+The destructuring assignment is a basically a JavaScript expression.
+It allows us to unpack the values from arrays or properties from objects
+into distinct variables.
+*/
+// /*syntax for array destructuring:    
+// const [element1, element2, ...rest] = array; 
