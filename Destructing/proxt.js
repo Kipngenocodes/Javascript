673b9690aa5600aa17a6e001fcc12b779239b67e
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-14 00:29:34 -0700

    working on proxies

---
+++ Destructing/proxt.js
@@ -0,0 +1,8 @@
+/*The JavaScript proxies are objects that allow you to wrap a
+particular object and customize the fundamental operations of the object, 
+like getting and setting object properties.
+*/
+// The proxies are used to implement features such as logging, caching, and securities.
+// The proxies are used to intercept the operations of an object and customize them.
+// The proxies are used to create a new object that wraps the original object and provides a new interface for it.
+// Sntax: const obj = new Proxy(targetObj, Handler);
