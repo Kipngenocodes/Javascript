69f5ddfd5a4bde547889b90e220bb3a9fd3c7ee2
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-19 23:44:59 -0700

    working with asynchronous javascript

---
+++ javascript_Asynchronous/asy_js.js
@@ -0,0 +1,6 @@
+/*Asynchronous JavaScript is a programming technique that 
+enables your program to start a potentially long-running task 
+and continue to executing other tasks parallelly. JavaScript is a single-threaded programming language.
+It means you can execute a single script or particular code at a time.
+JavaScript control flow moves the line by line and executes each line of code.*/
+//To perform the multiple tasks parallelly, you need asynchronous JavaScript.
