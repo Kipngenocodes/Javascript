0d27cfd9280994ac30abffc93ccc1a77917a1a59
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-04-19 23:50:11 -0700

    working on update

--- javascript_Asynchronous/asy_js.js
+++ javascript_Asynchronous/asy_js.js
@@ -4,3 +4,5 @@ and continue to executing other tasks parallelly. JavaScript is a single-threade
 It means you can execute a single script or particular code at a time.
 JavaScript control flow moves the line by line and executes each line of code.*/
 //To perform the multiple tasks parallelly, you need asynchronous JavaScript.
+//Asynchronous JavaScript is a programming technique that enables your program to start a potentially long-running task and continue to executing other tasks parallelly.
+//It is a way to perform non-blocking operations in JavaScript.
