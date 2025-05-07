8ce6cd1d7c85a923199f958b59584cb574da741e
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-05-07 04:40:08 -0700

    working with promises in javascript

---
+++ promise/promise.js
@@ -0,0 +1,8 @@
+// The promise is an object that represent the completion or failure of an asynchronous operations.
+// The promise object is used to handle asynchronous operations in JavaScript.
+// Can be created using the Promise constructor or by using the async/await syntax.
+// The promise object has three states: pending, fulfilled, and rejected.
+// The promise object has two methods: then() and catch().  
+// The callback function accepts two functions, resolve() and reject(), as arguments. 
+// The resolve function is called if the promise returns successfully. 
+// The reject function is called when taks fails and returns the reason.
