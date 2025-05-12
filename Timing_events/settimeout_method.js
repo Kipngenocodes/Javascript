d2dd8b664f6319065eae388c3fac6eea8ec10d30
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-05-11 22:26:48 -0700

    working on settimeout function

---
+++ Timing_events/settimeout_method.js
@@ -0,0 +1,32 @@
+// JavaScript timing events are used to execute the code after a specified time only once or multiple times.
+// JavaScript setTimeout() method is used to execute a function after a specified number of milliseconds.
+// setTimeout() method is used to execute a function after a specified number of milliseconds.  
+// The setTimeout() method is used to execute a function after a specified number of milliseconds.
+// ClearTimeout() method is used to cancel the execution of a function that has been scheduled using setTimeout() method.
+// the Clearinterval() method is used to cancel the execution of a function that has been scheduled using setInterval() method.
+
+// Example of setTimeout() method
+function greet() {
+    console.log("Hello, World!");
+}
+
+setTimeout(greet, 3000); // Executes greet() after 3 seconds (3000 milliseconds)
+
+// Example of setTimeout() with parameters
+function greetUser(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+
+// Example of setTimeout() with promise
+function greetUserWithPromise(name) { 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Hello, ${name}!`);
+        }, 2000);
+    });
+} 
+
+greetUserWithPromise("John").then((message) => {
+    console.log(message);
+});
