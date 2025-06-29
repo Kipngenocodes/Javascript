97e0beb583e463728fedf450cfa9d30f2a8a2d3d
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-06-29 12:58:44 -0700

    update

---
+++ javascript_miscellaneous/asynch/form_validation.js
@@ -0,0 +1,7 @@
+// Form validation normally used to occur at the server, after the client had entered all
+//  the necessary data and then pressed the Submit button. If the data entered by a client
+//  was incorrect or was simply missing, the server would have to send all the data back
+//  to the client and request that the form be resubmitted with correct information.
+//  This was really a lengthy process which used to put a lot of burden on the server.
+
+
